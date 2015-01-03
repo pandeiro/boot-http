e82d70761d011c0b455605f45f407da9cd111e59
Murphy McMahon <pandeiro@gmail.com>
2015-01-03 17:27:36 -0200

    add directory index handler

--- build.boot
+++ build.boot
@@ -1,27 +1,27 @@
 (set-env!
  :source-paths #{"src"}
  :dependencies '[[org.clojure/clojure "1.6.0"     :scope "provided"]
-                 [boot/core           "2.0.0-rc1" :scope "provided"]
+                 [boot/core           "2.0.0-rc5" :scope "provided"]
                  [adzerk/bootlaces    "0.1.5"     :scope "test"]])
 
 (require
- '[adzerk.bootlaces :refer :all]
+ '[adzerk.bootlaces :refer :all] ;; tasks: build-jar push-snapshot push-release
  '[boot.pod         :as pod]
  '[boot.util        :as util]
  '[boot.core        :as core])
 
-(def +version+ "0.3.0")
-
+(def +version+ "0.4.0-alpha")
 (bootlaces! +version+)
 
 (task-options!
- pom {:project 'pandeiro/boot-http
-      :version +version+
-      :description "Boot task to serve a directory over HTTP."
+ pom {:project     'pandeiro/boot-http
+      :version     +version+
+      :description "Boot task to serve HTTP."
       :url         "https://github.com/pandeiro/boot-http"
       :scm         {:url "https://github.com/pandeiro/boot-http"}
       :license     {:name "Eclipse Public License"
                     :url  "http://www.eclipse.org/legal/epl-v10.html"}})
 
-(require '[pandeiro.http :refer :all])
+(require
+ '[pandeiro.http :refer :all])
 
--- src/pandeiro/http.clj
+++ src/pandeiro/http.clj
@@ -3,38 +3,101 @@
   (:require
    [boot.pod           :as pod]
    [boot.util          :as util]
-   [boot.core          :as core]
+   [boot.core          :as core :refer [deftask]]
    [boot.task.built-in :as task]))
 
-(def ^:private deps
+(def ^:private serve-deps
   '[[ring/ring-jetty-adapter "1.3.1"]
-    [compojure "1.2.1"]])
+    [ring/ring-core "1.3.1"]])
 
-(core/deftask serve
-  "Start a web server on localhost and serve a directory, blocking
-   the boot task pipeline by default.
+(def default-port 3000)
 
-   If no directory is specified the current one is used.  Listens
-   on port 3000 by default."
-  [d dir      PATH     str  "The directory to serve."
-   p port     PORT     int  "The port to listen on."
-   b block             bool "Blocking (for standalone use)"]
-  (let [worker   (pod/make-pod (assoc-in (core/get-env) [:dependencies] deps))
-        dir      (or dir ".")
-        port     (or port 3000)
-        block    (or block false)]
+(deftask serve
+  "Start a web server on localhost, serving resources and optionally a directory.
+  Listens on port 3000 by default."
+
+  [d dir  PATH str "The directory to serve."
+   p port PORT int "The port to listen on. (Default: 3000)"]
+
+  (let [worker (pod/make-pod (assoc-in (core/get-env) [:dependencies] serve-deps))
+        port   (or port default-port)]
+
     (core/cleanup
      (util/info "\n<< stopping Jetty... >>\n")
      (pod/with-eval-in worker (.stop server)))
-    (comp
-     (core/with-pre-wrap fileset
-       (pod/with-eval-in worker
-         (require '[ring.adapter.jetty :refer [run-jetty]]
-                  '[compojure.route    :refer [files]])
-         (def server
-           (run-jetty (files "/" {:root ~dir}) {:port ~port :join? false})))
-       (util/info "<< started Jetty on http://localhost:%d (serving: %s) >>\n" port dir)
-       fileset)
-     (if block
-       (task/wait)
-       identity))))
+
+    (core/with-pre-wrap fileset
+      (pod/with-eval-in worker
+        (import  '[java.net URLEncoder URLDecoder])
+        (require '[clojure.java.io :as io]
+                 '[clojure.string  :as s]
+                 '[ring.adapter.jetty :refer [run-jetty]]
+                 '[ring.util response]
+                 '[ring.middleware file resource content-type not-modified])
+
+        ;;
+        ;; Directory serving
+        ;;
+        (def root-path
+          "Used to resolve URIs to filepaths when serving from a specific directory"
+          ~(if dir
+             (.getPath (io/file dir))
+             ""))
+
+        (defn path-diff [path]
+          (s/replace path (re-pattern (str "^" root-path)) ""))
+
+        (defn filepath-from-uri [uri]
+          (str root-path (java.net.URLDecoder/decode uri "UTF-8")))
+        
+        (defn list-item [file]
+          (format "<li><a href=\"%s\">%s</a></li>"
+                  (path-diff (.getPath file))
+                  (.getName file)))
+
+        (defn index-file-exists? [files]
+          (first (filter #(#{"index.html" "index.htm"} (.getName %)) files)))
+
+        (defn index [{:keys [uri] :as req}]
+          (let [directory (io/file (filepath-from-uri uri))]
+            (when (.exists directory)
+              (let [files (sort (.listFiles directory))]
+                {:status  200
+                 :headers {"Content-Type" "text/html"}
+                 :body    (if-let [index-file (index-file-exists? files)]
+                            (slurp index-file)
+                            (format (str "<!doctype html><meta charset=\"utf-8\">"
+                                         "<body><h1>Directory listing</h1><hr>"
+                                         "<ul>%s</ul></body>")
+                                    (apply str (map list-item files))))}))))
+
+        ;;
+        ;; Handlers
+        ;;
+        (defn resources [req]
+          (ring.middleware.resource/resource-request req ""))
+
+        (def directories-and-resources
+          (when ~dir
+            (-> index
+              (ring.middleware.file/wrap-file ~dir {:index-files? false})
+              (ring.middleware.resource/wrap-resource ""))))
+
+        (def handler
+          "If DIR present, serve directories and resources; otherwise just resources"
+          (if ~dir
+            directories-and-resources
+            resources))
+
+        ;;
+        ;; Jetty
+        ;;
+        (def server
+          (run-jetty (-> handler
+                       (ring.middleware.content-type/wrap-content-type)
+                       (ring.middleware.not-modified/wrap-not-modified))
+                     {:port ~port :join? false})))
+
+      (util/info "<< started Jetty on http://localhost:%d >>\n" port)
+      fileset)))
+
