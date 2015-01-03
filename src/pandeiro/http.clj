e6feefaa8cd707d60a67c9844ec5eb8190bc7e64
Murphy McMahon <pandeiro@gmail.com>
2015-01-03 19:14:21 -0200

    move require to inside pod, remove from parent ns

--- src/pandeiro/http.clj
+++ src/pandeiro/http.clj
@@ -4,14 +4,16 @@
    [boot.pod           :as pod]
    [boot.util          :as util]
    [boot.core          :as core :refer [deftask]]
-   [boot.task.built-in :as task]
-   [pandeiro.http.impl :as http]))
+   [boot.task.built-in :as task]))
 
 (def default-port 3000)
 
 (def serve-deps
   '[[ring/ring-jetty-adapter "1.3.1"]
     [ring/ring-core "1.3.1"]])
+
+(def serve-worker
+  (pod/make-pod (assoc-in (core/get-env) [:dependencies] serve-deps)))
 
 (deftask serve
   "Start a web server on localhost, serving resources and optionally a directory.
@@ -20,15 +22,16 @@
   [d dir  PATH str "The directory to serve."
    p port PORT int "The port to listen on. (Default: 3000)"]
 
-  (let [worker (pod/make-pod (assoc-in (core/get-env) [:dependencies] serve-deps))
-        port   (or port default-port)]
+  (let [port   (or port default-port)]
 
     (core/cleanup
      (util/info "\n<< stopping Jetty... >>\n")
-     (pod/with-eval-in worker (.stop server)))
+     (pod/with-eval-in serve-worker
+       (.stop server)))
 
     (core/with-pre-wrap fileset
-      (pod/with-call-in worker
+      (pod/with-eval-in serve-worker
+        (require '[pandeiro.http.impl :as http])
         (def server (http/server {:dir ~dir, :port ~port})))
       (util/info "<< started Jetty on http://localhost:%d >>\n" port)
       fileset)))
