06e5baa5e9fb7e396fe94a019a197e02773dc3f6
Murphy McMahon <pandeiro@gmail.com>
2015-01-03 18:52:05 -0200

    refactor out impl ns (wip)

--- src/pandeiro/http.clj
+++ src/pandeiro/http.clj
@@ -4,13 +4,14 @@
    [boot.pod           :as pod]
    [boot.util          :as util]
    [boot.core          :as core :refer [deftask]]
-   [boot.task.built-in :as task]))
-
-(def ^:private serve-deps
-  '[[ring/ring-jetty-adapter "1.3.1"]
-    [ring/ring-core "1.3.1"]])
+   [boot.task.built-in :as task]
+   [pandeiro.http.impl :as http]))
 
 (def default-port 3000)
+
+(def serve-deps
+  '[[ring/ring-jetty-adapter "1.3.1"]
+    [ring/ring-core "1.3.1"]])
 
 (deftask serve
   "Start a web server on localhost, serving resources and optionally a directory.
@@ -27,77 +28,8 @@
      (pod/with-eval-in worker (.stop server)))
 
     (core/with-pre-wrap fileset
-      (pod/with-eval-in worker
-        (import  '[java.net URLEncoder URLDecoder])
-        (require '[clojure.java.io :as io]
-                 '[clojure.string  :as s]
-                 '[ring.adapter.jetty :refer [run-jetty]]
-                 '[ring.util response]
-                 '[ring.middleware file resource content-type not-modified])
-
-        ;;
-        ;; Directory serving
-        ;;
-        (def root-path
-          "Used to resolve URIs to filepaths when serving from a specific directory"
-          ~(if dir
-             (.getPath (io/file dir))
-             ""))
-
-        (defn path-diff [path]
-          (s/replace path (re-pattern (str "^" root-path)) ""))
-
-        (defn filepath-from-uri [uri]
-          (str root-path (java.net.URLDecoder/decode uri "UTF-8")))
-        
-        (defn list-item [file]
-          (format "<li><a href=\"%s\">%s</a></li>"
-                  (path-diff (.getPath file))
-                  (.getName file)))
-
-        (defn index-file-exists? [files]
-          (first (filter #(#{"index.html" "index.htm"} (.getName %)) files)))
-
-        (defn index [{:keys [uri] :as req}]
-          (let [directory (io/file (filepath-from-uri uri))]
-            (when (.exists directory)
-              (let [files (sort (.listFiles directory))]
-                {:status  200
-                 :headers {"Content-Type" "text/html"}
-                 :body    (if-let [index-file (index-file-exists? files)]
-                            (slurp index-file)
-                            (format (str "<!doctype html><meta charset=\"utf-8\">"
-                                         "<body><h1>Directory listing</h1><hr>"
-                                         "<ul>%s</ul></body>")
-                                    (apply str (map list-item files))))}))))
-
-        ;;
-        ;; Handlers
-        ;;
-        (defn resources [req]
-          (ring.middleware.resource/resource-request req ""))
-
-        (def directories-and-resources
-          (when ~dir
-            (-> index
-              (ring.middleware.file/wrap-file ~dir {:index-files? false})
-              (ring.middleware.resource/wrap-resource ""))))
-
-        (def handler
-          "If DIR present, serve directories and resources; otherwise just resources"
-          (if ~dir
-            directories-and-resources
-            resources))
-
-        ;;
-        ;; Jetty
-        ;;
-        (def server
-          (run-jetty (-> handler
-                       (ring.middleware.content-type/wrap-content-type)
-                       (ring.middleware.not-modified/wrap-not-modified))
-                     {:port ~port :join? false})))
-
+      (pod/with-call-in worker
+        (def server (http/server {:dir ~dir, :port port})))
       (util/info "<< started Jetty on http://localhost:%d >>\n" port)
       fileset)))
 
---
+++ src/pandeiro/http/impl.clj
@@ -0,0 +1,64 @@
+(ns pandeiro.http.impl
+  (:import  [java.net URLDecoder])
+  (:require [clojure.java.io :as io]
+            [clojure.string  :as s]
+            [ring.middleware file resource content-type not-modified]
+            [ring.adapter.jetty :refer [run-jetty]]))
+
+;;
+;; Directory serving
+;;
+(def index-files #{"index.html" "index.htm"})
+
+(defn index-file-exists? [files]
+  (first (filter #(index-files (.getName %)) files)))
+
+(defn path-diff [root-path path]
+  (s/replace path (re-pattern (str "^" root-path)) ""))
+
+(defn filepath-from-uri [root-path uri]
+  (str root-path (URLDecoder/decode uri "UTF-8")))
+
+(defn list-item [root-path]
+  (fn [file]
+    (format "<li><a href=\"%s\">%s</a></li>"
+            (path-diff root-path (.getPath file))
+            (.getName file))))
+
+(defn index-for [dir]
+  (let [root-path (or (.getPath (io/file dir)) "")]
+    (fn [{:keys [uri] :as req}]
+      (let [directory (io/file (filepath-from-uri root-path uri))]
+        (when (.exists directory)
+          (let [files (sort (.listFiles directory))]
+            {:status  200
+             :headers {"Content-Type" "text/html"}
+             :body    (if-let [index-file (index-file-exists? files)]
+                        (slurp index-file)
+                        (format (str "<!doctype html><meta charset=\"utf-8\">"
+                                     "<body><h1>Directory listing</h1><hr>"
+                                     "<ul>%s</ul></body>")
+                                (apply str (map (list-item root-path) files))))}))))))
+
+;;
+;; Handlers
+;;
+(defn resources [req]
+  (ring.middleware.resource/resource-request req ""))
+
+(defn directories-and-resources [dir]
+  (-> (index-for dir)
+    (ring.middleware.file/wrap-file dir {:index-files? false})
+    (ring.middleware.resource/wrap-resource "")))
+
+;;
+;; Jetty
+;;
+(defn server [{:keys [dir port]}]
+  (let [handler (if dir
+                  (directories-and-resources dir)
+                  resources)]
+    (run-jetty (-> handler
+                 (ring.middleware.content-type/wrap-content-type)
+                 (ring.middleware.not-modified/wrap-not-modified))
+               {:port port :join? false})))
