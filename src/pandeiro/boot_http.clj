ad94976d1951db5448a18ca86fd5a90c08fb844d
Murphy McMahon <pandeiro@gmail.com>
2015-01-05 02:23:30 -0200

    create a worker for each task instance

--- src/pandeiro/boot_http.clj
+++ src/pandeiro/boot_http.clj
@@ -12,9 +12,6 @@
   '[[ring/ring-jetty-adapter "1.3.1"]
     [ring/ring-core "1.3.1"]])
 
-(def serve-worker
-  (pod/make-pod (update-in (core/get-env) [:dependencies] into serve-deps)))
-
 (deftask serve
   "Start a web server on localhost, serving resources and optionally a directory.
   Listens on port 3000 by default."
@@ -25,15 +22,19 @@
    p port    PORT int "The port to listen on. (Default: 3000)"]
 
   (let [port   (or port default-port)
-        server (pod/with-eval-in serve-worker (gensym))
+        worker (pod/make-pod (update-in (core/get-env) [:dependencies]
+                                        into serve-deps))
         start  (delay
-                 (pod/with-eval-in serve-worker
+                 (pod/with-eval-in worker
                    (require '[pandeiro.boot-http.impl :as http])
-                   (def ~server
-                     (http/server {:dir ~dir, :port ~port, :handler '~handler :resource-root ~resource-root})))
+                   (def server
+                     (http/server {:dir ~dir, :port ~port, :handler '~handler
+                                   :resource-root ~resource-root})))
                  (util/info "<< started Jetty on http://localhost:%d >>\n" port))]
     (core/cleanup
       (util/info "\n<< stopping Jetty... >>\n")
-      (pod/with-eval-in serve-worker
-        (.stop ~server)))
-    (core/with-pre-wrap fileset @start fileset)))
+      (pod/with-eval-in worker
+        (.stop server)))
+    (core/with-pre-wrap fileset
+      @start
+      fileset)))
